f35fed6d0bb0ec325a4e2497f4295cb00465eb31
lautaroalvarez11 <lealvarez485@comunidad.unnoba.edu.ar>
2025-11-07 12:04:59 -0300

    .

--- tpIntegrador/tp-integrador/src/components/Seccion2JQuery.jsx
+++ tpIntegrador/tp-integrador/src/components/Seccion2JQuery.jsx
@@ -22,20 +22,29 @@ export default function Seccion2JQuery() {
         alt="img1"
         style={{ width: "100px", height: "100px", objectFit: "cover", borderRadius: "8px" }}
         />
+
         <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
         alt="img2"
         style={{ width: "100px", height: "100px", objectFit: "cover", borderRadius: "8px" }}
         />
+
         <img
         src="https://cdn.iconscout.com/icon/free/png-512/free-jquery-icon-svg-download-png-1175155.png?f=webp&w=512"
         alt="img3"
         style={{ width: "100px", height: "100px", objectFit: "cover", borderRadius: "8px" }}
         />
+
         <img
         src="https://miro.medium.com/v2/1*CPF3XWLLwtr43AU37kQwrA.jpeg"
         alt="img4"
         style={{ width: "200px", height: "100px", objectFit: "cover", borderRadius: "8px" }}
+        />
+
+        <img
+        src="https://uploads.sitepoint.com/wp-content/uploads/2016/10/1477673935anime-logo.png"
+        alt="img5"
+        style={{ width: "400px", height: "100px", objectFit: "cover", borderRadius: "8px" }}
         />
       </div>
 
